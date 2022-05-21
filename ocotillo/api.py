b989439d0c82d060ee6c750a3ad88f58ee5285d9
Piotr Dabkowski <piodrus@gmail.com>
2022-05-21 13:41:11 +0200

    make it possible to get character timings from the Transcriber, improve audio chunking

--- ocotillo/api.py
+++ ocotillo/api.py
@@ -6,98 +6,120 @@ import torchaudio
 from ocotillo.model_loader import load_model
 from ocotillo.utils import load_audio
 
-def _append_with_at_least_one_space(text, new_text):
-    if not text.endswith(' ') and not new_text.startswith(' '):
-        text = text + " "
-    return text + new_text
 
 class Transcriber:
-    def __init__(self, phonetic=False, on_cuda=True, cuda_device=0):
-        if on_cuda:
+    def __init__(self, phonetic=False, cuda_device=-1, use_torchscript=False):
+        if cuda_device >= 0:
             self.device = f'cuda:{cuda_device}'
         else:
             self.device = 'cpu'
-        self.model, self.processor = load_model(self.device, phonetic=phonetic)
+        self.model, self.processor = load_model(self.device, phonetic=phonetic, use_torchscript=use_torchscript)
+        self.model.eval()
 
-    def transcribe(self, audio_data, sample_rate):
-        """
-        Transcribes audio_data at the given sample_rate. audio_data is expected to be a list of floats, a torch tensor
-        or a numpy array. audio_data must be either 1d mono audio data or 2d stereo data (one channel is thrown out).
-        The channel dimension must be first.
-        """
-        if not isinstance(audio_data, torch.Tensor):
-            audio_data = torch.tensor(audio_data, dtype=torch.float)
-        audio_data = audio_data.to(self.device).unsqueeze(0)
-        if audio_data.shape[-1] > 30 * sample_rate:
-            return self._process_large_clip(audio_data, sample_rate)
-        else:
-            return self.transcribe_batch(audio_data, sample_rate)[0]
-
-    def transcribe_batch(self, audio_data, sample_rate):
-        """
-        Transcribes audio_data at the given sample_rate. audio_data is expected to be a torch tensor
-        or a numpy array. audio_data must be either 2d mono audio data or 3d stereo data (one channel is thrown out).
-        The batch dimension is first. The channel dimension is second. transcribe_batch() will not handle audio clips
-        of length > 30 seconds. Transcribe long clips individually using transcribe.
-        """
-        if not isinstance(audio_data, torch.Tensor):
-            audio_data = torch.tensor(audio_data, dtype=torch.float)  # This makes valid inputs either a torch tensor a numpy array.
-        if sample_rate != 16000:
-            audio_data = torchaudio.functional.resample(audio_data, sample_rate, 16000)
-        assert audio_data.shape[-1] < 30 * 16000
-        audio_data = audio_data.to(self.device)
-        clip_norm = (audio_data - audio_data.mean()) / torch.sqrt(audio_data.var() + 1e-7)
+    def transcribe_batch(self, wave_b1t, sample_rate):
+        wave_b1t = self._prepare_wave(wave_b1t, sample_rate)
         with torch.no_grad():
-            logits = self.model(clip_norm)[0]
-            tokens = torch.argmax(logits, dim=-1)
-        return self.processor.batch_decode(tokens)
+            logits_btc = self.model(wave_b1t.squeeze(1))[0]
+            tokens = torch.argmax(logits_btc, dim=-1)
+        return self.processor.batch_decode(tokens), logits_btc
 
-    def _process_large_clip(self, audio_data, sample_rate):
-        # Handle extra-long clips by breaking them into chunks with 2 seconds of shared audio on each end.
+    def _prepare_wave(self, wave_b1t, sample_rate):
+        assert len(wave_b1t.shape) == 3, wave_b1t.shape
+        assert wave_b1t.shape[1] == 1, wave_b1t.shape
         if sample_rate != 16000:
-            audio_data = torchaudio.functional.resample(audio_data, sample_rate, 16000)
+            wave_b1t = torchaudio.functional.resample(wave_b1t, sample_rate, 16000)
+        # Normalize the wave.
+        wave_b1t = (wave_b1t - wave_b1t.mean(2, keepdim=True)) / torch.sqrt(wave_b1t.var(2, keepdim=True) + 1e-7)
+        wave_b1t = wave_b1t.to(self.device)
+        return wave_b1t
+
+    def transcribe_batch_with_time_chunking(self, wave_b1t, sample_rate, chunk_secs=22, overlap_secs=2):
+        assert chunk_secs > overlap_secs, f"chunk_secs must be longer than 2*overlap_secs: {chunk_secs} > 2*{overlap_secs}"
+        if chunk_secs >= (wave_b1t.shape[2] / sample_rate - 0.1):
+            return self.transcribe_batch(wave_b1t, sample_rate=sample_rate)
+        assert isinstance(chunk_secs, int)
+        assert isinstance(overlap_secs, int)
         wav2vec_logit_unit = 320
-        chunk_sz = wav2vec_logit_unit * 1000  # Roughly 20 seconds
-        logits_overlap_sz = 100
-        overlap_sz = wav2vec_logit_unit * logits_overlap_sz  # Roughly 2 seconds
+        chunk_sz = wav2vec_logit_unit * chunk_secs * 50
+        logits_overlap_sz = 50 * overlap_secs
+        logits_sz = 50 * chunk_secs
+        overlap_sz = wav2vec_logit_unit * logits_overlap_sz
+
+        original_batch_size = wave_b1t.shape[0]
+        wave_b1t = self._prepare_wave(wave_b1t, sample_rate)
         chunked_clips = []
-        i = 0
-        while i < audio_data.shape[-1]:
-            chunked_clips.append(audio_data[:, max(0, i - overlap_sz):i + chunk_sz])
-            i += chunk_sz
-        # Pad the first and last elements so they can be batched together.
-        chunked_clips[0] = torch.nn.functional.pad(chunked_clips[0], (overlap_sz,0))
-        chunked_clips[-1] = torch.nn.functional.pad(chunked_clips[-1],(0,overlap_sz+chunk_sz-chunked_clips[-1].shape[-1]))
+        start = 0
+        while 1:
+            end = start + chunk_sz
+            chunked_clips.append(wave_b1t[:, :, start:end])
+            if end >= wave_b1t.shape[-1]:
+                break
+            start += chunk_sz - overlap_sz
+
+        # Pad the last element to chunk_sz
+        chunked_clips[-1] = torch.nn.functional.pad(chunked_clips[-1],
+                                                    (0, chunk_sz - chunked_clips[-1].shape[-1]))
         chunked_clips = torch.cat(chunked_clips, dim=0)
-        # Further split up the tensor into sub-batches that can fit in device memory. <8> is arbitrarily chosen
-        # because it can fit in 12GiB of GPU memory.
-        chunked_clips = torch.split(chunked_clips, 8, dim=0)
-        text_out = ""
+        chunked_clips = torch.split(chunked_clips, original_batch_size, dim=0)
+
+        logit_chunks = []
         last_logits = None
-        for batch in chunked_clips:
+        for wave_b1t_chunk in chunked_clips:
             with torch.no_grad():
-                batch_norm = (batch - batch.mean()) / torch.sqrt(batch.var() + 1e-7)
-                batched_logits = self.model(batch_norm)[0]
-                for logits in batched_logits:
-                    if last_logits is None:
-                        last_logits = logits
-                    else:
-                        # Use the overlap to finalize last_logits into text.
-                        last_logits[-logits_overlap_sz:] = (last_logits[-logits_overlap_sz:] + logits[:logits_overlap_sz]) / 2
-                        tokens = torch.argmax(last_logits, dim=-1)
-                        text_out = _append_with_at_least_one_space(text_out, self.processor.decode(tokens))
-                        last_logits = logits[logits_overlap_sz:]
-        # Don't forget about the remaining logits!
-        tokens = torch.argmax(last_logits, dim=-1)
-        text_out = _append_with_at_least_one_space(text_out, self.processor.decode(tokens))
-        return text_out
+                logits_chunk_btc = self.model(wave_b1t_chunk.squeeze(1))[0]
+                if logits_chunk_btc.shape[1] != logits_sz:
+                    # wav2vec2 returns 1 less time step than expected for 50hz.
+                    logits_chunk_btc = torch.nn.functional.interpolate(logits_chunk_btc.permute(0, 2, 1),
+                                                                       [logits_sz], mode="nearest").permute(0, 2, 1)
+
+                if last_logits is None:
+                    last_logits = logits_chunk_btc
+                else:
+                    # Use the overlap to finalize last_logits into text.
+                    last_logits[:, -logits_overlap_sz:] = (last_logits[:, -logits_overlap_sz:] + logits_chunk_btc[:,
+                                                                                                 :logits_overlap_sz]) / 2
+                    logit_chunks.append(last_logits)
+                    last_logits = logits_chunk_btc[:, logits_overlap_sz:]
+        logit_chunks.append(last_logits)
+        logits_btc = torch.cat(logit_chunks, dim=1)
+        return self.processor.batch_decode(torch.argmax(logits_btc, dim=-1)), logits_btc
+
+    def logits_to_text_with_timings(self, logits_tc):
+        """
+        Returns text and the start time for each character in text. The timings are quite accurate.
+        """
+        tokens = torch.argmax(logits_tc, dim=-1).detach().cpu()
+        str_tokens = self.processor.tokenizer.convert_ids_to_tokens(tokens, skip_special_tokens=False)
+        result = []
+        result_timings = []
+        seen_pad = False
+        for i, token in enumerate(str_tokens):
+            if token == self.processor.tokenizer.pad_token:
+                seen_pad = True
+                continue
+            if not result or result[-1] != token or seen_pad:
+                seen_pad = False
+                assert len(token) == 1, token
+                result.append(token)
+                result_timings.append(i / 50.0)
+        return "".join(result).replace(self.processor.tokenizer.word_delimiter_token, " "), result_timings
 
 
 if __name__ == '__main__':
-    transcriber = Transcriber(on_cuda=True, phonetic=True)
+    transcriber = Transcriber(phonetic=False)
     audio = load_audio('../data/obama.mp3', 44100)
-    print(transcriber.transcribe(audio, 44100))
-    start = time()
-    audio = load_audio('../data/obama_long.mp3', 16000)
-    print(transcriber.transcribe(audio, 16000))
-    print(f"Elapsed: {time() - start}")
+    audio = audio.unsqueeze(0).unsqueeze(0)
+
+    text, logits_btc = (transcriber.transcribe_batch(torch.cat([audio, ], dim=0), 44100))
+    print(text)
+    # Now dump the alignment to textgrid, it can be easily viewed via praat.
+    import textgrid
+    grid = textgrid.TextGrid()
+    char_grid = textgrid.IntervalTier()
+    chars, times = transcriber.logits_to_text_with_timings(logits_btc[0])
+    times.append(audio.shape[-1]/44100)
+    for i in range(len(chars)):
+        char_grid.add(minTime=times[i], maxTime=times[i+1], mark=chars[i])
+    grid.append(char_grid)
+    grid.write(open("../data/obama.textgrid", "w"))
+
